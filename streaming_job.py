8873a938c3c5d565efdb39d8da2f0af1a52f6b57
AbdelrahmanMalek <amm2592000@gmail.com>
2026-05-05 12:21:07 +0300

    Initial baseline pipeline: Kafka → Spark → PostgreSQL → Power BI

---
+++ streaming_job.py
@@ -0,0 +1,153 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StringType, DoubleType, IntegerType
+from pyspark.sql.functions import to_timestamp
+from pyspark.sql.types import TimestampType
+from pyspark.sql.functions import when
+from pyspark.sql.functions import window, avg
+
+spark = SparkSession.builder \
+    .master("local[*]") \
+    .appName("KafkaTest") \
+    .config(
+        "spark.jars.packages",
+        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0"
+    ) \
+    .config(
+        "spark.jars",
+        "/home/jovyan/jars/postgresql-42.6.2.jar"
+    ) \
+    .getOrCreate()
+
+
+df = spark.readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "kafka:9092") \
+    .option("subscribe", "iot-data") \
+    .option("startingOffsets", "earliest") \
+    .load()
+
+
+schema = StructType() \
+    .add("device_id", StringType()) \
+    .add("device_type", StringType()) \
+    .add("location", StringType()) \
+    .add("technician", StringType()) \
+    .add("temperature", DoubleType()) \
+    .add("humidity", DoubleType()) \
+    .add("status", StringType()) \
+    .add("timestamp", StringType())
+
+parsed_df = df.selectExpr("CAST (value AS String)") \
+    .select(from_json(col("value"), schema).alias("data")) \
+    .select("data.*")
+
+parsed_df = parsed_df.withColumn(
+    "timestamp",
+    to_timestamp(col("timestamp"))
+)
+
+clean_df = parsed_df \
+    .filter(col("device_id").isNotNull())\
+    .filter(col("timestamp").isNotNull())\
+    .filter(col("temperature").between(-20, 80))\
+    .filter(col("humidity").between(0, 100))
+
+
+clean_df = clean_df.withColumn(
+    "anomaly_flag",
+    when(col("temperature") > 50, 1).otherwise(0)
+)
+
+query_parquet = clean_df.writeStream \
+    .format("parquet") \
+    .option("path", "/home/jovyan/data/output") \
+    .option("checkpointLocation", "/home/jovyan/data/checkpoints/parquet") \
+    .partitionBy("device_id") \
+    .outputMode("append") \
+    .start()
+
+schema_parquet = StructType() \
+    .add("device_id", StringType()) \
+    .add("device_type", StringType()) \
+    .add("location", StringType()) \
+    .add("technician", StringType()) \
+    .add("temperature", DoubleType()) \
+    .add("humidity", DoubleType()) \
+    .add("status", StringType()) \
+    .add("timestamp", TimestampType())\
+    .add("anomaly_flag", IntegerType())
+
+
+
+
+df_parquet_stream = spark.readStream \
+    .format("parquet") \
+    .schema(schema_parquet) \
+    .load("/home/jovyan/data/output")
+
+agg_df = df_parquet_stream \
+    .withWatermark("timestamp", "1 minute")\
+    .groupBy(
+        col("device_id"),
+        window(col("timestamp"), "1 minute")
+    ) \
+    .agg(
+        avg("temperature").alias("avg_temp"),
+        avg("humidity").alias("avg_humidity")
+    )
+
+
+def write_raw(batch_df, batch_id):
+    print(f"[PARQUET → POSTGRES] Batch {batch_id}, rows: {batch_df.count()}")
+
+    batch_df.select(
+        "device_id",
+        "device_type",
+        "location",
+        "technician",
+        "temperature",
+        "humidity",
+        "status",
+        "timestamp",
+        "anomaly_flag"
+    ).write \
+        .format("jdbc") \
+        .option("url", "jdbc:postgresql://postgres:5432/db") \
+        .option("dbtable", "iot_data") \
+        .option("user", "user") \
+        .option("password", "password") \
+        .option("driver", "org.postgresql.Driver") \
+        .mode("append") \
+        .save()
+    
+def write_agg(batch_df, batch_id):
+    batch_df = batch_df.select(
+        col("device_id"),
+        col("window.start").alias("window_start"),
+        col("window.end").alias("window_end"),
+        col("avg_temp"),
+        col("avg_humidity")
+    )
+
+    batch_df.write \
+        .format("jdbc") \
+        .option("url", "jdbc:postgresql://postgres:5432/db") \
+        .option("dbtable", "iot_aggregates") \
+        .option("user", "user") \
+        .option("password", "password") \
+        .option("driver", "org.postgresql.Driver") \
+        .mode("append") \
+        .save()
+
+raw_query = df_parquet_stream.writeStream \
+    .foreachBatch(write_raw) \
+    .option("checkpointLocation", "/home/jovyan/data/checkpoints/raw_data") \
+    .start()
+
+agg_query = agg_df.writeStream \
+    .foreachBatch(write_agg) \
+    .option("checkpointLocation", "/home/jovyan/data/checkpoints/agg_data") \
+    .start()
+
+spark.streams.awaitAnyTermination()
